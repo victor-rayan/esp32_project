172243ed1da0ffaace2f39aed168d1e9619b173a
PauloAbiAcl <pv.silva23@hotmail.com>
2023-07-15 02:04:30 -0300

    controleLuz

--- main/ligaTela.c
+++ main/ligaTela.c
@@ -1,19 +1,32 @@
 #include "driver/touch_pad.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <driver/gpio.h>
 
 #define TOUCH_THRESHOLD     700
 #define TOUCH_FILTER_VALUE  10
 #define TOUCH_DELAY_MS      200
+
+#define LED_GPIO_PIN  GPIO_NUM_2
+
+void controleLuzEsp(bool acao)
+{
+  esp_rom_gpio_pad_select_gpio(LED_GPIO_PIN );
+  gpio_set_direction(LED_GPIO_PIN , GPIO_MODE_OUTPUT);
+  if(acao)
+    gpio_set_level(LED_GPIO_PIN , 1);
+  else
+    gpio_set_level(LED_GPIO_PIN , 0);
+}
 
 void touchTask(void *pvParameters) {
     touch_pad_intr_enable();
     while (1) {
         uint32_t pad_status = touch_pad_get_status();
         if (pad_status & (1 << TOUCH_PAD_NUM0)) {
-            printf("Pad 1\n");
+            controleLuzEsp(true);
             touch_pad_clear_status();
-
+            
             // Desabilita temporariamente a detecção de toque
             touch_pad_config(TOUCH_PAD_NUM0, 0);
             vTaskDelay(pdMS_TO_TICKS(TOUCH_DELAY_MS)); // Pequeno atraso para evitar detecção múltipla
@@ -30,3 +43,4 @@ void initTouch() {
     touch_pad_config(TOUCH_PAD_NUM0, TOUCH_THRESHOLD);
     touch_pad_filter_start(TOUCH_FILTER_VALUE);
 }
+
