48f23b4fd5f3aaa5c1626c95acb762f3e755cc91
PauloAbiAcl <pv.silva23@hotmail.com>
2023-07-18 00:15:38 -0300

    paulo carrega mula

--- main/mqtt/json_parser.c
+++ main/mqtt/json_parser.c
@@ -8,62 +8,52 @@
 
 int corLed;
 
-int json_parse_return_comm(const char *const json)
-{
-  if (strstr(json, "azul"))
-  {
+int json_parse_return_comm(const char *const json) {
+  if (strstr(json, "azul")) {
     corLed = BLUE;
     return BLUE;
   }
 
-  if (strstr(json, "verde"))
-  {
+  if (strstr(json, "verde")) {
     corLed = GREEN;
     return GREEN;
   }
 
-  if (strstr(json, "vermelho"))
-  {
+  if (strstr(json, "vermelho")) {
     corLed = RED;
     return RED;
   }
 
-  if (strstr(json, "amarelo"))
-  {
+  if (strstr(json, "amarelo")) {
     corLed = YELLOW;
     return YELLOW;
   }
 
-  if (strstr(json, "ardosia"))
-  {
+  if (strstr(json, "ardosia")) {
     corLed = ARDOSIA;
     printf("ardosia");
     return ARDOSIA;
   }
 
-  if (strstr(json, "magenta"))
-  {
+  if (strstr(json, "magenta")) {
     corLed = MAGENTA;
     printf("magenta");
     return MAGENTA;
   }
 
-  if (strstr(json, "floresta"))
-  {
+  if (strstr(json, "floresta")) {
     printf("floresta");
     corLed = FLORESTA;
     return FLORESTA;
   }
 
-  if (strstr(json, "chocolate"))
-  {
+  if (strstr(json, "chocolate")) {
     corLed = CHOCOLATE;
     printf("chocolate");
     return CHOCOLATE;
   }
 
-  if (strstr(json, "on"))
-  {
+  if (strstr(json, "on")) {
     printf("ligar/desligar");
     return TURN_SYSTEM;
   }
