5de3aae99f9cb32eca9b859e3c840214c82b9f77
Victor Rayan <victor.rayan378@gmail.com>
2023-07-18 22:50:20 -0300

    Update app_main.c

--- main/app_main.c
+++ main/app_main.c
@@ -1,88 +1,98 @@
-#include <stdio.h>
-#include <string.h>
-#include "nvs_flash.h"
-#include "esp_wifi.h"
-#include "esp_event.h"
-#include "esp_http_client.h"
-#include "esp_log.h"
-#include "freertos/semphr.h"
-#include <stdlib.h>
-#include <time.h>
-#include <sys/time.h>
+#include <stdbool.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_sleep.h"
-#include "esp32/rom/uart.h"
-#include <driver/gpio.h>
-#include "driver/rtc_io.h"
-#include "luzQuarto.h"
-#include "driver/touch_pad.h"
+#include "freertos/queue.h"
+#include "esp_system.h"
+#include "esp_log.h"
 
-#include "wifi.h"
-#include "mqtt.h"
-#include "ligaTela.h"
-#include "rotary_encoder.h"
-#include <stdio.h>
 #include "rotary_encoder.h"
 
-#define ROT_ENC_A_GPIO 25 // Pino GPIO para sinal A do encoder
-#define ROT_ENC_B_GPIO 27 // Pino GPIO para sinal B do encoder
-#define ENABLE_HALF_STEPS true // Ativar meio passo (opcional, conforme necessário)
-#define MAX_TEMPERATURE 100 // Temperatura máxima permitida (exemplo)
+#define TAG "app"
 
-// Variáveis globais para armazenar informações do encoder
-// Definir função MAX para obter o valor máximo entre dois números
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+#define ROT_ENC_A_GPIO 14
+#define ROT_ENC_B_GPIO 15
 
-rotary_encoder_info_t info;
-int32_t rotaryPosition;
-int32_t referenceTemperature;
+#define ENABLE_HALF_STEPS false  // Set to true to enable tracking of rotary encoder at half step resolution
+#define RESET_AT          0      // Set to a positive non-zero number to reset the position if this value is exceeded
+#define FLIP_DIRECTION    false  // Set to true to reverse the clockwise/counterclockwise sense
 
-// Definir função MAX para obter o valor máximo entre dois números
-#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+// Lista de números
+int numeros[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+int num_numeros = sizeof(numeros) / sizeof(numeros[0]);
 
-// Função para ajustar a temperatura do sistema com base na referência
-void adjustTemperature() {
-    // Implemente aqui o código para ajustar o sistema de acordo com a referenceTemperature.
-    // Por exemplo, ligue ou desligue um sistema de aquecimento/resfriamento, ajuste a velocidade do ventilador, etc.
-    printf("Ajustando a temperatura para: %ld\n", referenceTemperature);
+// Variável para armazenar a posição atual
+int posicao_atual = 0;
+
+// Funções de ação quando girar para a direita e para a esquerda
+void onEncoderRotateRight() {
+    posicao_atual++;
+    if (posicao_atual >= num_numeros) {
+        posicao_atual = 0;
+    }
+    printf("Girou para a direita! Número correspondente: %d\n", numeros[posicao_atual]);
 }
 
-void app_main(void) {
-    // esp32-rotary-encoder requer que o serviço de ISR do GPIO seja instalado antes de chamar rotary_encoder_register()
+void onEncoderRotateLeft() {
+    posicao_atual--;
+    if (posicao_atual < 0) {
+        posicao_atual = num_numeros - 1;
+    }
+    printf("Girou para a esquerda! Número correspondente: %d\n", numeros[posicao_atual]);
+}
+
+void app_main()
+{
+    // esp32-rotary-encoder requires that the GPIO ISR service is installed before calling rotary_encoder_register()
     ESP_ERROR_CHECK(gpio_install_isr_service(0));
 
-    // Inicializa o dispositivo encoder rotativo com os pinos GPIO para os sinais A e B
+    // Initialise the rotary encoder device with the GPIOs for A and B signals
+    rotary_encoder_info_t info = { 0 };
     ESP_ERROR_CHECK(rotary_encoder_init(&info, ROT_ENC_A_GPIO, ROT_ENC_B_GPIO));
     ESP_ERROR_CHECK(rotary_encoder_enable_half_steps(&info, ENABLE_HALF_STEPS));
+#ifdef FLIP_DIRECTION
+    ESP_ERROR_CHECK(rotary_encoder_flip_direction(&info));
+#endif
 
-    // Cria uma fila para eventos do driver do encoder rotativo.
-    // Tarefas podem ler desta fila para receber informações de posição atualizadas.
-    QueueHandle_t event_queue = xQueueCreate(10, sizeof(rotary_encoder_event_t));
-    info.state.position = 0;
-    rotaryPosition = info.state.position;
-    referenceTemperature = rotaryPosition * 5; // Cada clique do encoder ajusta a temperatura em 5 graus
-    printf("Posição inicial: %ld\n", rotaryPosition);
-    ESP_ERROR_CHECK(rotary_encoder_set_queue(&info, (QueueHandle_t)event_queue));
+    // Create a queue for events from the rotary encoder driver.
+    // Tasks can read from this queue to receive up to date position information.
+    QueueHandle_t event_queue = rotary_encoder_create_queue();
+    ESP_ERROR_CHECK(rotary_encoder_set_queue(&info, event_queue));
 
-    // Loop principal
-    while (1) {
-        // Aguarde um evento da fila do encoder rotativo
-        rotary_encoder_event_t event;
-        if (xQueueReceive(event_queue, &event, portMAX_DELAY) == pdTRUE) {
-            // Evento recebido, atualize a posição do encoder e a temperatura de referência
-            rotaryPosition += event.position;
-            rotaryPosition = MAX(0, rotaryPosition); // Garante que a posição não seja negativa
-            referenceTemperature = rotaryPosition * 5; // Cada clique do encoder ajusta a temperatura em 5 graus
-
-            // Verifique os limites da temperatura
-            if (referenceTemperature > MAX_TEMPERATURE) {
-                referenceTemperature = MAX_TEMPERATURE;
+    while (1)
+    {
+        // Verifica se há eventos no queue
+        rotary_encoder_event_t event = { 0 };
+        if (xQueueReceive(event_queue, &event, 1000 / portTICK_PERIOD_MS) == pdTRUE)
+        {
+            // Ação de acordo com a direção do encoder
+            if (event.state.direction == ROTARY_ENCODER_DIRECTION_CLOCKWISE) {
+                onEncoderRotateRight();
+            } else if (event.state.direction == ROTARY_ENCODER_DIRECTION_COUNTER_CLOCKWISE) {
+                onEncoderRotateLeft();
             }
 
-            // Atualiza o sistema para a nova temperatura de referência
-            adjustTemperature();
-            printf("Nova posição: %ld, Temperatura de referência: %ld\n", rotaryPosition, referenceTemperature);
+            ESP_LOGI(TAG, "Event: position %ld, direction %s", event.state.position,
+                     event.state.direction ? (event.state.direction == ROTARY_ENCODER_DIRECTION_CLOCKWISE ? "CW" : "CCW") : "NOT_SET");
+        }
+        else
+        {
+            // Poll current position and direction
+            rotary_encoder_state_t state = { 0 };
+            ESP_ERROR_CHECK(rotary_encoder_get_state(&info, &state));
+            ESP_LOGI(TAG, "Poll: position %ld, direction %s", state.position,
+                     state.direction ? (state.direction == ROTARY_ENCODER_DIRECTION_CLOCKWISE ? "CW" : "CCW") : "NOT_SET");
+
+            // Reset the device
+            if (RESET_AT && (state.position >= RESET_AT || state.position <= -RESET_AT))
+            {
+                ESP_LOGI(TAG, "Reset");
+                ESP_ERROR_CHECK(rotary_encoder_reset(&info));
+            }
         }
     }
+
+    ESP_LOGE(TAG, "queue receive failed");
+
+    ESP_ERROR_CHECK(rotary_encoder_uninit(&info));
 }
